a51f2e85a772354ecda81405468e73b4401e7e96
EllisOllier <ellisollier1@gmail.com>
2025-11-04 13:11:09 +0000

    Added registration functionality with supabase auth

---
+++ app/auth/register/route.ts
@@ -0,0 +1,27 @@
+'use server'
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import type { NextRequest } from "next/server";
+
+export async function POST(
+    _req: NextRequest
+){
+    const supabase = await createClient();
+
+    // TODO: Validate request body
+    const body = await _req.json()
+    const data = { // very basic validation
+        email: body.email as string,
+        password: body.password as string,
+    }
+    
+    const { error } = await supabase.auth.signUp(data);
+
+    if(error){
+        redirect('/error')
+    }
+
+    revalidatePath('/', 'layout')
+    redirect('/profile')
+}

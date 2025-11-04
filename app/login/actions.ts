c2a6e73a93e4872d5dec32f371f14c9c1ce4de13
EllisOllier <ellisollier1@gmail.com>
2025-11-04 13:42:19 +0000

    Added error logging to console

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -15,6 +15,7 @@ export async function login(formData: FormData){
     const { error } = await supabase.auth.signInWithPassword(data);
 
     if(error){
+        console.log(error)
         redirect('/error')
     }
 
@@ -34,6 +35,7 @@ export async function signup(formData: FormData){
     const { error } = await supabase.auth.signUp(data);
 
     if(error){
+        console.log(error)
         redirect('/error')
     }
 

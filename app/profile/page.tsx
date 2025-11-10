70056cc8d9e99186c4f7493623239be68ce4799a
EllisOllier <ellisollier1@gmail.com>
2025-11-10 15:27:12 +0000

    Added form to add goals to database

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,72 @@
+'use client';
+
+import { useState } from "react";
+
 export default function ProfilePage() {
+    const [item, setItem ] = useState({
+        title: "",
+        description: "",
+        target_amount: 0.00,
+        category: "",
+        deadline: "", // Might have to convert string to date
+    });
+
+    const [loading, setLoading ] = useState(false);
+    
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+        try{
+            const res = fetch('/api/goals', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(item),
+            });
+        } catch (err: any){
+            alert(err.message) // Change to a red message under form
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
         <div>
             <h1>Welcome to your profile!</h1>
+            
+            <h2>Add a Goal:</h2>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    placeholder="Title"
+                    value={item.title}
+                    onChange={(e) => setItem({ ...item, title: e.target.value })}
+                />
+                <input 
+                    placeholder="Description"
+                    value={item.description}
+                    onChange={(e) => setItem({ ...item, description: e.target.value })}
+                />
+                <input 
+                    placeholder="Target Amount"
+                    type="number"
+                    min={0}
+                    max={100}
+                    value={item.target_amount}
+                    onChange={(e) => setItem({ ...item, target_amount: Number(e.target.value) })}
+                />
+                <input 
+                    placeholder="Category"
+                    value={item.category}
+                    onChange={(e) => setItem({ ...item, category: e.target.value })}
+                />
+                <input 
+                    type="date"
+                    value={item.deadline}
+                    onChange={(e) => setItem({ ...item, deadline: e.target.value })}
+                />
+                <button type="submit" disabled={loading}>
+                    {loading ? "Adding..." : "Add Goal"}
+                </button>
+            </form>
         </div>
-    )
+    );
 }
